c4877d2a5e6ac5eba8f12e2584017e9706afe756
Nikolaos Margaris <reverence23@gmail.com>
2015-07-29 00:55:11 +0300

    Add morefor calls

--- public/js/controllers/calls.js
+++ public/js/controllers/calls.js
@@ -3,12 +3,21 @@ controllers.controller('CallsCtrl',
 		function ($scope, CallSvc) {
 			console.log('CallsCtrl started');
 
+			// interface
+			$scope.addCall = function () {
+				CallSvc.addCall($scope.call, function (err) {
+					error && console.log('Error creating new call' + err);
+				});
+			};
+
+			// Events
 			$scope.$on('$destroy', function () {
 				console.log('CallsCtrl destroyed');
 			});
 
 			// Initialization
 			$scope.calls = [];
+			$scope.call = new Call();
 
 			CallSvc.getCalls(function (err,reply) {
 				if (err) {
